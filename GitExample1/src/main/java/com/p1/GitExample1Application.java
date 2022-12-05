4691481d3b1a8fd0cba8e93218948cd975136c82
nikshithareddy <nikshithaaella123@gmail.com>
2022-12-05 16:51:50 +0530

    HII

---
+++ GitExample1/src/main/java/com/p1/GitExample1Application.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExample1Application.class, args);
+	}
+
+}
